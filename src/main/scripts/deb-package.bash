4ce55d46142c89819b6ba17447e09496792a2058
Sylvain Hellegouarch <sylvain@atomist.com>
2016-11-29 16:55:26 +0100

    Moved deb/rpm scripts
    Moved deb/rpm scripts from build to src directory.
    Fixed bash code style and structure.

--- src/main/scripts/deb-package.bash
+++ src/main/scripts/deb-package.bash
@@ -20,18 +20,38 @@ function install_deps() {
 
 function gitentry2debentry() {
     local previous=$1
-    local version=$2
+    if [ -z ${previous+x} ]; then
+        err "missing required argument: previous"
+        return 10
+    fi
+    shift
+
+    local version=$1
+    if [ -z ${version+x} ]; then
+        err "missing required argument: version"
+        return 10
+    fi
 
     echo "rug-cli ($version) unstable; urgency=low"
     echo
-    git --no-pager log --format="  * %s" $previous${previous:+..}$version
+    if ! git --no-pager log --format="  * %s" $previous${previous:+..}$version; then 
+        err "Failed to extract logs"
+        return 1
+    fi
     echo
-    git --no-pager log --format=" -- $Author  %aD" -n 1 $version
+    if ! git --no-pager log --format=" -- $Author  %aD" -n 1 $version; then
+        err "Failed to extract author"
+        return 1
+    fi
     echo
 }
 
 function git2deb_changelog() {
     local project_version=$1
+    if [[ ! $project_version ]]; then
+        err "missing required argument: project_version"
+        return 10
+    fi
 
     local current_lang=$LANG
     if [[ "${LANG}" != "en_US.UTF-8" ]]; then
@@ -44,24 +64,37 @@ function git2deb_changelog() {
     # iterate over all release tag and generate debian changelog stanzas
     # borrowed from https://gist.github.com/hfs/0ff1cf243b163bd551ec22604ce702a5/
     local tags=$(git tag --sort "-version:refname" | grep -E -o "^[0-9]+\.[0-9]+\.[0-9]+$")
-    if [ "${tags}" = "" ]; then
+    if [[ -z "${tags}" ]]; then
         # no tags there yet, so let's pretend
         echo "No tags found"
-        gitentry2debentry "" "" > target/linux/deb/changelog
-        sed -i "s/()/($project_version)/" target/linux/deb/changelog
+        if ! gitentry2debentry "" "" > target/linux/deb/changelog; then
+            err "Failed to create single changelog entry"
+            return 1
+        fi
+        if ! sed -i "s/()/($project_version)/" target/linux/deb/changelog; then
+            err "Failed setting project version in changelog"
+            return 1
+        fi
     else
-        echo "$tags" | {
+        if ! echo "$tags" | {
             read version; while read previous; do
-                    gitentry2debentry $previous $version
+                    if ! gitentry2debentry $previous $version; then
+                        err "Failed to create changelog entry"
+                        return 1
+                    fi
                     version="$previous"
             done
-            gitentry2debentry "" $version
-        } > target/linux/deb/changelog
+            if ! gitentry2debentry "" $version; then 
+                err "Failed to create first changelog entry"
+                return 1
+            fi
+        } > target/linux/deb/changelog; then
+            err "Failed to create changelog"
+            return 1
+        fi
     fi
 
     LANG=$current_lang
-
-    return 0
 }
 
 function create_directory_structure() {
@@ -69,34 +102,54 @@ function create_directory_structure() {
     # be deployed on target machines
     echo "Creating directory structure..."
     rm -rf target/linux/deb
-    mkdir -p target/linux/deb/rug-cli
-    mkdir -p target/linux/deb/rug-cli/usr/share/bash-completion/completions
-    mkdir -p target/linux/deb/rug-cli/usr/share/doc/rug-cli
-    mkdir -p target/linux/deb/rug-cli/usr/share/rug/bin
-    mkdir -p target/linux/deb/rug-cli/usr/share/rug/lib
-
-    return $?
+    mkdir -p target/linux/deb/rug-cli/usr/share/{bash-completion/completions,doc/rug-cli,rug/{bin,lib}}
 }
 
 function copy_files() {
     local project_version=$1
+    if [[ ! $project_version ]]; then
+        err "missing required argument: project_version"
+        return 10
+    fi
+
     local source_dir="target/rug-cli-$project_version-bin/rug-cli-$project_version"
     local dest_dir="target/linux/deb/rug-cli/usr/share"
 
-    echo "ln -s /usr/share/rug/bin/rug /usr/bin/rug" > postinst
-    echo "rm /usr/bin/rug" > preuninst
-    cp build/debian/copyright $dest_dir/doc/rug-cli/copyright
-    cp $source_dir/bin/rug $dest_dir/rug/bin/rug
-    cp $source_dir/lib/rug-cli-$project_version.jar $dest_dir/rug/lib/rug-cli-$project_version.jar
-    cp $source_dir/etc/bash_completion.d/rug $dest_dir/bash-completion/completions/rug
-
-    return 0
+    if ! echo "ln -s /usr/share/rug/bin/rug /usr/bin/rug" > postinst; then 
+        err "Failed to create postinst file"
+        return 1
+    fi
+    if ! echo "rm /usr/bin/rug" > preuninst; then
+        err "Failed to create preuninst file"
+        return 1
+    fi
+    if ! cp src/main/scripts/debian/copyright $dest_dir/doc/rug-cli/copyright; then
+        err "Failed to copy debian copyright file"
+        return 1
+    fi
+    if ! cp src/main/bash/etc/bash_completion.d/rug $dest_dir/bash-completion/completions/rug; then 
+        err "Failed to copy bash completion file"
+        return 1
+    fi
+    if ! cp $source_dir/bin/rug $dest_dir/rug/bin/rug; then
+        err "Failed to copy rug binary"
+        return 1
+    fi
+    if ! cp $source_dir/lib/rug-cli-$project_version.jar $dest_dir/rug/lib/rug-cli-$project_version.jar; then 
+        err "Failed to copy rug jar file"
+        return 1
+    fi
 }
 
 function upload_to_repository() {
     echo "Uploading to repository..."
 
     local package=$1
+    if [[ ! $package ]]; then
+        err "missing required argument: package"
+        return 10
+    fi
+
     local debian_repo_url="https://atomist.jfrog.io/atomist/debian/pool"
     local distributions="deb.distribution=wheezy;deb.distribution=jessie;deb.distribution=yakkety;deb.distribution=xenial;deb.distribution=wily;deb.distribution=vivid"
     local components="deb.component=main"
@@ -115,12 +168,14 @@ function upload_to_repository() {
         err $response
         return 1
     fi
-    
-    return 0
 }
 
 function create_package() {
     local project_version=$1
+    if [[ ! $project_version ]]; then
+        err "missing required argument: project_version"
+        return 10
+    fi
 
     # generate the debian package
     echo "Creating package..."
@@ -139,8 +194,6 @@ function create_package() {
         --after-install=postinst \
         --pre-uninstall=preuninst \
         -C target/linux/deb/rug-cli
-
-    return $?
 }
 
 function main() {
@@ -176,7 +229,10 @@ function main() {
         return 1
     fi
 
-    copy_files $project_version
+    if ! copy_files $project_version; then
+        err "failed to copy required resources"
+        return 1
+    fi
 
     if ! create_package $project_version; then
         err "failed to create debian package"
@@ -190,8 +246,6 @@ function main() {
     fi
 
     echo "created and upload $project_version"
-
-    return 0
 }
 
 main "$@" || exit 1
--- src/main/scripts/rpm-package.bash
+++ src/main/scripts/rpm-package.bash
@@ -20,15 +20,35 @@ function install_deps() {
 
 function gitentry2rpmentry() {
     local previous=$1
-    local version=$2
+    if [ -z ${previous+x} ]; then
+        err "missing required argument: previous"
+        return 10
+    fi
+    shift
 
-    git --no-pager log --date="format:%a %b %d %Y" --format="* %ad $Author $version" -n 1 $version
-    git --no-pager log --format="- %s" $previous${previous:+..}$version
+    local version=$1
+    if [ -z ${version+x} ]; then
+        err "missing required argument: version"
+        return 10
+    fi
+
+    if ! git --no-pager log --date="format:%a %b %d %Y" --format="* %ad $Author $version" -n 1 $version; then 
+        err "Failed to extract logs"
+        return 1
+    fi
+    if ! git --no-pager log --format="- %s" $previous${previous:+..}$version; then
+        err "Failed to extract author"
+        return 1
+    fi
     echo
 }
 
 function git2rpm_changelog() {
     local project_version=$1
+    if [[ ! $project_version ]]; then
+        err "missing required argument: project_version"
+        return 10
+    fi
 
     local current_lang=$LANG
     if [[ "${LANG}" != "en_US.UTF-8" ]]; then
@@ -38,27 +58,40 @@ function git2rpm_changelog() {
         LANG="en_US.UTF-8"
     fi
 
-    # iterate over all release tag and generate debian changelog stanzas
+    # iterate over all release tag and generate RPM changelog stanzas
     # borrowed from https://gist.github.com/hfs/0ff1cf243b163bd551ec22604ce702a5/
     local tags=$(git tag --sort "-version:refname" | grep -E -o "^[0-9]+\.[0-9]+\.[0-9]+$")
-    if [ "${tags}" = "" ]; then
+    if [[ -z "${tags}" ]]; then
         # no tags there yet, so let's pretend
         echo "No tags found"
-        gitentry2rpmentry "" "" > target/linux/rpm/changelog
-        sed -i "s/()/($project_version)/" target/linux/rpm/changelog
+        if ! gitentry2rpmentry "" "" > target/linux/rpm/changelog; then
+            err "Failed to create single changelog entry"
+            return 1
+        fi
+        if ! sed -i "s/()/($project_version)/" target/linux/rpm/changelog; then
+            err "Failed setting project version in changelog"
+            return 1
+        fi
     else
-        echo "$tags" | {
+        if ! echo "$tags" | {
             read version; while read previous; do
-                    gitentry2rpmentry $previous $version
+                    if ! gitentry2rpmentry $previous $version; then
+                        err "Failed to create changelog entry"
+                        return 1
+                    fi
                     version="$previous"
             done
-            gitentry2rpmentry "" $version
-        } > target/linux/rpm/changelog
+            if ! gitentry2rpmentry "" $version; then 
+                err "Failed to create first changelog entry"
+                return 1
+            fi
+        } > target/linux/rpm/changelog; then
+            err "Failed to create changelog"
+            return 1
+        fi
     fi
 
     LANG=$current_lang
-    
-    return 0
 }
 
 function create_directory_structure() {
@@ -66,33 +99,50 @@ function create_directory_structure() {
     # be deployed on target machines
     echo "Creating directory structure..."
     rm -rf target/linux/rpm/
-    mkdir -p target/linux/rpm/rug-cli
-    mkdir -p target/linux/rpm/rug-cli/bash-completion/completions
-    mkdir -p target/linux/rpm/rug-cli/doc/rug-cli
-    mkdir -p target/linux/rpm/rug-cli/rug/bin
-    mkdir -p target/linux/rpm/rug-cli/rug/lib
-
-    return $?
+    mkdir -p target/linux/rpm/rug-cli/{bash-completion/completions,doc/rug-cli,rug/{bin,lib}}
 }
 
 function copy_files() {
     local project_version=$1
+    if [[ ! $project_version ]]; then
+        err "missing required argument: project_version"
+        return 10
+    fi
+
     local source_dir="target/rug-cli-$project_version-bin/rug-cli-$project_version"
     local dest_dir="target/linux/rpm/rug-cli"
 
-    echo "ln -s /usr/share/rug/bin/rug /usr/bin/rug" > postinst
-    echo "rm /usr/bin/rug" > preuninst
-    cp $source_dir/bin/rug $dest_dir/rug/bin/rug
-    cp $source_dir/lib/rug-cli-$project_version.jar $dest_dir/rug/lib/rug-cli-$project_version.jar
-    cp $source_dir/etc/bash_completion.d/rug $dest_dir/bash-completion/completions/rug
-
-    return 0
+    if ! echo "ln -s /usr/share/rug/bin/rug /usr/bin/rug" > postinst; then 
+        err "Failed to create postinst file"
+        return 1
+    fi
+    if ! echo "rm /usr/bin/rug" > preuninst; then
+        err "Failed to create preuninst file"
+        return 1
+    fi
+    if ! cp src/main/bash/etc/bash_completion.d/rug $dest_dir/bash-completion/completions/rug; then 
+        err "Failed to copy bash completion file"
+        return 1
+    fi
+    if ! cp $source_dir/bin/rug $dest_dir/rug/bin/rug; then
+        err "Failed to copy rug binary"
+        return 1
+    fi
+    if ! cp $source_dir/lib/rug-cli-$project_version.jar $dest_dir/rug/lib/rug-cli-$project_version.jar; then 
+        err "Failed to copy rug jar file"
+        return 1
+    fi
 }
 
 function upload_to_repository() {
     echo "Uploading to repository..."
 
     local package=$1
+    if [[ ! $package ]]; then
+        err "missing required argument: package"
+        return 10
+    fi
+
     local yum_repo_url="https://atomist.jfrog.io/atomist/yum"
     local checksum=$(sha1sum $package | awk '{ print $1 }')
 
@@ -108,12 +158,14 @@ function upload_to_repository() {
         err $response
         return 1
     fi
-    
-    return 0
 }
 
 function create_package() {
     local project_version=$1
+    if [[ ! $project_version ]]; then
+        err "missing required argument: project_version"
+        return 10
+    fi
 
     # generate the RPM package
     echo "Creating package..."
@@ -135,8 +187,6 @@ function create_package() {
         --after-install=postinst \
         --pre-uninstall=preuninst \
         -C target/linux/rpm/rug-cli
-
-    return $?
 }
 
 function main() {
@@ -172,7 +222,10 @@ function main() {
         return 1
     fi
 
-    copy_files $project_version
+    if ! copy_files $project_version; then
+        err "failed to copy required resources"
+        return 1
+    fi
 
     if ! create_package $project_version; then
         err "failed to create rpm package"
@@ -187,8 +240,6 @@ function main() {
     fi
 
     echo "created and upload $project_version"
-
-    return 0
 }
 
 main "$@" || exit 1
