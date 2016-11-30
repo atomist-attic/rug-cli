08fe45ef00ef8f295d7c9e40685c0ee32040027d
Christian Dupuis <cd@atomist.com>
2016-11-30 10:43:38 +0100

    Add -w to wait for input at start time
    This makes it easier to attach a debugger or profiler

--- src/main/java/com/atomist/rug/cli/Main.java
+++ src/main/java/com/atomist/rug/cli/Main.java
@@ -1,23 +1,37 @@
 package com.atomist.rug.cli;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import com.atomist.rug.cli.command.ServiceLoadingCommandInfoRegistry;
 import com.atomist.rug.cli.output.ConsoleUtils;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        args = waitForInput(args);
+        
         configureEnv();
         configureStreams();
 
         new Runner(new ServiceLoadingCommandInfoRegistry()).run(args);
     }
 
-    private static void configureStreams() {
-        ConsoleUtils.configureStreams();
+    private static String[] waitForInput(String[] args) throws IOException {
+        if (args.length > 0 && args[args.length - 1].equals("-w")) {
+            System.in.read();
+            args = Arrays.copyOfRange(args, 0, args.length - 1);
+        }
+        return args;
     }
 
     private static void configureEnv() {
         System.setProperty("java.awt.headless", Boolean.toString(true));
     }
+
+    private static void configureStreams() {
+        ConsoleUtils.configureStreams();
+    }
+
 
 }
