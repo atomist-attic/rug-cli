53ed27f3d54a0e2f0f976fbf245dbcc370fbcb40
Christian Dupuis <cd@atomist.com>
2016-12-29 18:03:49 +0100

    Change to use Java 8 functions

--- src/main/java/com/atomist/rug/cli/command/utils/ArtifactSourceUtils.java
+++ src/main/java/com/atomist/rug/cli/command/utils/ArtifactSourceUtils.java
@@ -9,39 +9,28 @@ import java.util.List;
 import com.atomist.rug.cli.RunnerException;
 import com.atomist.rug.resolver.ArtifactDescriptor;
 import com.atomist.source.ArtifactSource;
-import com.atomist.source.DirectoryArtifact;
-import com.atomist.source.FileArtifact;
 import com.atomist.source.file.FileSystemArtifactSource;
 import com.atomist.source.file.SimpleFileSystemArtifactSourceIdentifier;
 import com.atomist.source.file.ZipFileArtifactSourceReader;
 import com.atomist.source.file.ZipFileInput;
 
-import scala.runtime.AbstractFunction1;
-
 public abstract class ArtifactSourceUtils {
 
     public static final List<String> DIRS_TO_FILTER = Arrays.asList(".git");
-    public static final List<String> FILES_TO_FILTER = Arrays
-            .asList(".DS_Store", ".travis.yml", "travis-build.bash");
+    public static final List<String> FILES_TO_FILTER = Arrays.asList(".DS_Store", ".travis.yml",
+            "travis-build.bash");
 
     public static ArtifactSource filter(ArtifactSource source) {
-        return source.filter(new AbstractFunction1<DirectoryArtifact, Object>() {
+        ArtifactSource result = source.filter(
+                (d) -> !ArtifactSourceUtils.DIRS_TO_FILTER.contains(d.name()),
+                (f) -> !ArtifactSourceUtils.FILES_TO_FILTER.contains(f.name()));
 
-            @Override
-            public Object apply(DirectoryArtifact dir) {
-                return !ArtifactSourceUtils.DIRS_TO_FILTER
-                        .contains(dir.name());
-            }
-        }, new AbstractFunction1<FileArtifact, Object>() {
-
-            @Override
-            public Object apply(FileArtifact file) {
-                return !ArtifactSourceUtils.FILES_TO_FILTER
-                        .contains(file.name());
-            }
-        });
+//        ArtifactSourceTreeCreator.visitTree(result,
+//                new LogVisitor(new Log(ArtifactSourceUtils.class)));
+        
+        return result;
     }
-    
+
     public static ArtifactSource createArtifactSource(ArtifactDescriptor artifact) {
         try {
             File archiveRoot = new File(artifact.uri());
