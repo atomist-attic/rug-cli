fcfe3eaeeff10d3ffdacd08f30e4f01ef8afd280
Christian Dupuis <cd@atomist.com>
2017-01-02 12:47:03 +0100

    Remove node_modules from classpath

--- src/main/java/com/atomist/rug/cli/command/AbstractLocalArtifactDescriptorProvider.java
+++ src/main/java/com/atomist/rug/cli/command/AbstractLocalArtifactDescriptorProvider.java
@@ -1,16 +1,9 @@
 package com.atomist.rug.cli.command;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Options;
 
-import com.atomist.rug.cli.utils.CommandLineOptions;
 import com.atomist.rug.manifest.Manifest;
 import com.atomist.rug.manifest.ManifestArtifactDescriptorCreator;
 import com.atomist.rug.manifest.ManifestFactory;
@@ -20,7 +13,7 @@ import com.atomist.source.file.FileSystemArtifactSource;
 import com.atomist.source.file.SimpleFileSystemArtifactSourceIdentifier;
 
 public abstract class AbstractLocalArtifactDescriptorProvider extends AbstractCommandInfo
-        implements ArtifactDescriptorProvider, ClasspathEntryProvider {
+        implements ArtifactDescriptorProvider {
 
     public AbstractLocalArtifactDescriptorProvider(Class<? extends Command> commandClass,
             String commandName) {
@@ -44,40 +37,5 @@ public abstract class AbstractLocalArtifactDescriptorProvider extends AbstractCo
 
         throw new CommandException("No manifest.yml or package.json found in .atomist folder",
                 (String) null);
-    }
-
-    @Override
-    public List<URL> classpathEntries(ArtifactDescriptor artifact) {
-        Options options = options();
-        if (options.hasOption("l") && CommandLineOptions.hasOption("l")) {
-            return addNodeModulesToClasspath();
-        }
-        else if (!options.hasOption("l")) {
-            return addNodeModulesToClasspath();
-        }
-        return Collections.emptyList();
-    }
-
-    private List<URL> addNodeModulesToClasspath() {
-        File currentWorkingRoot = CommandUtils.getRequiredWorkingDirectory();
-        File nodeModuleRoot = new File(CommandUtils.getRequiredWorkingDirectory(), ".atomist/node_modules");
-        List<URL> urls = new ArrayList<>();
-        if (nodeModuleRoot.exists()) {
-            try {
-                urls.add(nodeModuleRoot.toURI().toURL());
-            }
-            catch (MalformedURLException e) {
-                // Can't happen
-            }
-        }
-        if (currentWorkingRoot.exists()) {
-            try {
-                urls.add(currentWorkingRoot.toURI().toURL());
-            }
-            catch (MalformedURLException e) {
-                // Can't happen
-            }
-        }
-        return urls;
     }
 }
