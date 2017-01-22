7e5da8cc0a8797974b9c96cb01a95766fdd84c44
Sylvain Hellegouarch <sylvain@atomist.com>
2017-01-22 09:58:12 +0100

    Suggest Java 8 to be installed but do not enforce it #70

--- src/main/scripts/deb-package.bash
+++ src/main/scripts/deb-package.bash
@@ -187,7 +187,7 @@ function create_package() {
         --url https://www.atomist.com \
         --license "GPL-3.0" \
         --vendor "Atomist, Inc" \
-        -d "openjdk-8-jdk" \
+        --deb-suggests "openjdk-8-jdk" \
         --deb-changelog target/linux/deb/changelog \
         --after-install=postinst \
         --pre-uninstall=preuninst \
