b1ee5aac873db41194ef36552f5ee6a32e5017c6
Christian Dupuis <cd@atomist.com>
2017-01-25 12:23:08 +0100

    Allow for . periods in group ids
    fixes #84

--- src/test/java/com/atomist/rug/cli/RugArchiveLifecycleTest.java
+++ src/test/java/com/atomist/rug/cli/RugArchiveLifecycleTest.java
@@ -20,7 +20,7 @@ public class RugArchiveLifecycleTest extends AbstractCommandTest {
     @Test
     public void testAGenerate() throws Exception {
         assertSuccess("Successfully generated new project my-rug-archive", "generate", "atomist-rugs:rug-archive:NewRugArchiveProject",
-                "my-rug-archive", "group_id=atomist-rugs", "version=0.1.0",
+                "my-rug-archive", "group_id=atomist.rugs", "version=0.1.0",
                 "description=My first Rug Archive project", "-C", location);
     }
 
@@ -31,21 +31,26 @@ public class RugArchiveLifecycleTest extends AbstractCommandTest {
     }
 
     @Test
-    public void testCTest() throws Exception {
+    public void testCDescribe() throws Exception {
         IOUtils.copy(new FileInputStream("../cli.yml"),
                 new FileOutputStream(new File(location, "cli.yml")));
         System.setProperty("user.dir", location + File.separator + "my-rug-archive");
+        assertSuccess("To get more information on any of the Rugs listed above, run", "describe", "archive", "-l");
+    }
+
+    @Test
+    public void testDTest() throws Exception {
         assertSuccess("Successfully executed 3 of 3 scenarios: Test SUCCESS", "test");
     }
 
     @Test
-    public void testDInstall() throws Exception {
-        assertSuccess("Successfully installed archive for atomist-rugs:my-rug-archive", "install");
+    public void testEInstall() throws Exception {
+        assertSuccess("Successfully installed archive for atomist.rugs:my-rug-archive", "install");
     }
 
     @Test
-    public void testDPublish() throws Exception {
-        assertSuccess("Successfully published archive for atomist-rugs:my-rug-archive", "publish");
+    public void testFPublish() throws Exception {
+        assertSuccess("Successfully published archive for atomist.rugs:my-rug-archive", "publish");
     }
     
     @AfterClass
