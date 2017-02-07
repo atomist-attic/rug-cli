c755de326a1731c43fd6764a92688dbe8f8fe5db
Christian Dupuis <cd@atomist.com>
2017-02-07 15:08:05 +0100

    Use classpath of local archive if possible
    All commands should use the classpath defined by a local archive if such archive exists.

--- src/main/java/com/atomist/rug/cli/command/AbstractRugScopedCommandInfo.java
+++ src/main/java/com/atomist/rug/cli/command/AbstractRugScopedCommandInfo.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,21 +12,31 @@ import org.apache.commons.cli.CommandLine;
 
 import com.atomist.rug.resolver.ArtifactDescriptor;
 import com.atomist.rug.resolver.ArtifactDescriptor.Extension;
+import com.atomist.rug.resolver.ArtifactDescriptorFactory;
 import com.atomist.rug.resolver.DefaultArtifactDescriptor;
 
-public abstract class AbstractRugScopedCommandInfo extends AbstractCommandInfo
+public abstract class AbstractRugScopedCommandInfo extends AbstractLocalArtifactDescriptorProvider
         implements ArtifactDescriptorProvider {
 
     private static final String RUG_VERSION = ".*<rug.version>(.*)<\\/rug.version>.*";
 
-    public AbstractRugScopedCommandInfo(Class<? extends Command> commandClass,
-            String commandName) {
+    public AbstractRugScopedCommandInfo(Class<? extends Command> commandClass, String commandName) {
         super(commandClass, commandName);
     }
 
     @Override
     public ArtifactDescriptor artifactDescriptor(CommandLine commandLine) {
+        Optional<ArtifactDescriptor> artifact = localArtifactDescriptor(commandLine);
         String version = readRugVersionFromPom();
+        if (artifact.isPresent()) {
+            // We are not interested in the operations right now; we only need the classpath
+            ArtifactDescriptor ad = artifact.get();
+            return ArtifactDescriptorFactory.copyFrom(ad, ad.group(), ad.artifact(), ad.version(),
+                    Extension.JAR);
+        }
+        else {
+            version = readRugVersionFromPom();
+        }
         return new DefaultArtifactDescriptor("com.atomist", "rug", version, Extension.JAR);
     }
 
