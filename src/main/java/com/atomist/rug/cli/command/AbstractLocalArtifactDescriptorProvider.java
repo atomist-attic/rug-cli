56d49ce8ccdafb98dc65fd4e54a711fd4bf0ff9d
Christian Dupuis <cd@atomist.com>
2017-02-07 15:08:05 +0100

    Use classpath of local archive if possible
    All commands should use the classpath defined by a local archive if such archive exists.

--- src/main/java/com/atomist/rug/cli/command/AbstractLocalArtifactDescriptorProvider.java
+++ src/main/java/com/atomist/rug/cli/command/AbstractLocalArtifactDescriptorProvider.java
@@ -1,6 +1,7 @@
 package com.atomist.rug.cli.command;
 
 import java.io.File;
+import java.util.Optional;
 
 import org.apache.commons.cli.CommandLine;
 
@@ -8,6 +9,7 @@ import com.atomist.rug.cli.command.utils.ArtifactSourceUtils;
 import com.atomist.rug.manifest.Manifest;
 import com.atomist.rug.manifest.ManifestArtifactDescriptorCreator;
 import com.atomist.rug.manifest.ManifestFactory;
+import com.atomist.rug.manifest.MissingManifestException;
 import com.atomist.rug.resolver.ArtifactDescriptor;
 import com.atomist.source.ArtifactSource;
 
@@ -20,6 +22,14 @@ public abstract class AbstractLocalArtifactDescriptorProvider extends AbstractCo
     }
 
     public ArtifactDescriptor artifactDescriptor(CommandLine commandLine) {
+        Optional<ArtifactDescriptor> artifact = localArtifactDescriptor(commandLine);
+
+        return artifact.orElseThrow(() -> new CommandException(
+                "No manifest.yml found in .atomist folder. Please add a manifest.yml file and run the command again.",
+                (String) null));
+    }
+
+    protected Optional<ArtifactDescriptor> localArtifactDescriptor(CommandLine commandLine) {
         String version = commandLine.getOptionValue('a');
         if (version == null) {
             version = "latest";
@@ -27,13 +37,17 @@ public abstract class AbstractLocalArtifactDescriptorProvider extends AbstractCo
 
         File projectRoot = CommandUtils.getRequiredWorkingDirectory();
         ArtifactSource source = ArtifactSourceUtils.createManifestOnlyArtifactSource(projectRoot);
-
-        Manifest manifest = ManifestFactory.read(source);
-        if (manifest != null) {
-            return new ManifestArtifactDescriptorCreator().create(manifest, projectRoot.toURI());
+        
+        try {
+            Manifest manifest = ManifestFactory.read(source);
+            if (manifest != null) {
+                return Optional.of(new ManifestArtifactDescriptorCreator().create(manifest,
+                        projectRoot.toURI()));
+            }
         }
-
-        throw new CommandException("No manifest.yml found in .atomist folder. Please add a manifest.yml file and run the command again.",
-                (String) null);
+        catch (MissingManifestException e) {
+            // We accept the fact that the manifest is missing or invalid
+        }
+        return Optional.empty();
     }
 }
