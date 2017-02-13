552c2611e5bc7553fe1c64b69bf35d3f4886b058
Christian Dupuis <cd@atomist.com>
2017-02-13 19:23:59 +0100

    First version of shell support (#105)
    * First attempt
    
    * Now with history and tab completion
    
    * Make repl into shell and make it a command
    
    * Break cycle
    
    * More shell changes
    
    * Fix a message typo (#104)
    
    it said: "Please verify that specified archive exists in at least on of your configured repositories."
    
    while I was in there:
    add a few toStrings that were useful in debugging
    use a long option name, so I can find it in search
    
    * Extract help command
    
    * Proper command parsing
    
    * Minor polish

--- src/main/java/com/atomist/rug/cli/command/ReflectiveCommandRunner.java
+++ src/main/java/com/atomist/rug/cli/command/ReflectiveCommandRunner.java
@@ -1,21 +1,32 @@
 package com.atomist.rug.cli.command;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.cli.CommandLine;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.UserInterruptException;
 
+import com.atomist.rug.cli.Log;
 import com.atomist.rug.cli.RunnerException;
 import com.atomist.rug.cli.classloading.ClassLoaderFactory;
 import com.atomist.rug.cli.classloading.ClasspathEntryProvider;
+import com.atomist.rug.cli.command.shell.ChangeDirCompleter;
+import com.atomist.rug.cli.command.shell.CommandInfoCompleter;
+import com.atomist.rug.cli.command.shell.OperationCompleter;
+import com.atomist.rug.cli.command.shell.ShellUtils;
+import com.atomist.rug.cli.command.utils.CommandHelpFormatter;
 import com.atomist.rug.cli.command.utils.DependencyResolverExceptionProcessor;
 import com.atomist.rug.cli.output.ProgressReporter;
 import com.atomist.rug.cli.output.ProgressReportingOperationRunner;
 import com.atomist.rug.cli.resolver.DependencyResolverFactory;
 import com.atomist.rug.cli.utils.ArtifactDescriptorUtils;
+import com.atomist.rug.cli.utils.Timing;
 import com.atomist.rug.cli.version.VersionUtils;
 import com.atomist.rug.resolver.ArtifactDescriptor;
 import com.atomist.rug.resolver.ArtifactDescriptorFactory;
@@ -24,30 +35,72 @@ import com.atomist.rug.resolver.DependencyResolverException;
 
 public class ReflectiveCommandRunner {
 
+    private final Log log = new Log(ReflectiveCommandRunner.class);
     private final CommandInfoRegistry registry;
 
     public ReflectiveCommandRunner(CommandInfoRegistry registry) {
         this.registry = registry;
     }
 
-    public void runCommand(String[] args, CommandLine commandLine) {
+    public int runCommand(String[] args, CommandLine commandLine) {
 
-        // Validate the JDK version
-        VersionUtils.validateJdkVersion();
+        Timing timing = new Timing();
 
-        ArtifactDescriptor artifact = null;
-        List<ArtifactDescriptor> dependencies = Collections.emptyList();
+        if (commandLine.hasOption("?") || commandLine.hasOption("h")) {
+            printCommandHelp(commandLine);
+            return 0;
+        }
 
+        List<ArtifactDescriptor> dependencies = new ArrayList<>();
         CommandInfo info = registry.findCommand(commandLine);
 
+        ArtifactDescriptor artifact = loadArtifactAndinitializeEnvironment(commandLine,
+                dependencies, info);
+        int rc = invokeCommand(args, artifact, dependencies, timing);
+
+        if (rc == 0 && "shell".equals(info.name())) {
+            invokeCommandInLoop(artifact, dependencies);
+        }
+
+        return rc;
+    }
+
+    private void invokeCommandInLoop(ArtifactDescriptor artifact, List<ArtifactDescriptor> dependencies) {
+        LineReader reader = ShellUtils.lineReader(ShellUtils.SHELL_HISTORY,
+                new ChangeDirCompleter(), new OperationCompleter(),
+                new CommandInfoCompleter(registry));
+        while (true) {
+            String line = null;
+            try {
+                line = reader.readLine(ShellUtils.DEFAULT_PROMPT);
+
+                if ("exit".equals(line.trim()) || "q".equals(line.trim())) {
+                    throw new EndOfFileException();
+                }
+
+                String[] args = CommandUtils.splitCommandline(line);
+                invokeCommand(args, artifact, dependencies, null);
+            }
+            catch (UserInterruptException e) {
+            }
+            catch (EndOfFileException e) {
+                log.info("Goodbye!");
+                return;
+            }
+        }
+    }
+
+    private ArtifactDescriptor loadArtifactAndinitializeEnvironment(CommandLine commandLine,
+            List<ArtifactDescriptor> dependencies, CommandInfo info) {
+        ArtifactDescriptor artifact = null;
         if (info instanceof ArtifactDescriptorProvider) {
             ArtifactDescriptor rootArtifact = ((ArtifactDescriptorProvider) info)
                     .artifactDescriptor(commandLine);
 
-            dependencies = new ProgressReportingOperationRunner<List<ArtifactDescriptor>>(
+            dependencies.addAll(new ProgressReportingOperationRunner<List<ArtifactDescriptor>>(
                     String.format("Resolving dependencies for %s",
-                            ArtifactDescriptorUtils.coordinates(rootArtifact)))
-                                    .run(indicator -> resolveDependencies(rootArtifact, indicator));
+                            ArtifactDescriptorUtils.coordinates(rootArtifact))).run(
+                                    indicator -> resolveDependencies(rootArtifact, indicator)));
 
             // Validate that this CLI version is compatible with declared version of Rug
             VersionUtils.validateRugCompatibility(rootArtifact, dependencies);
@@ -59,22 +112,87 @@ public class ReflectiveCommandRunner {
 
             // Setup the new classloader for the command to execute in
             if (info instanceof ClasspathEntryProvider) {
-                ClassLoaderFactory.setupClassLoader(artifact, dependencies, (ClasspathEntryProvider) info);
+                ClassLoaderFactory.setupClassLoader(artifact, dependencies,
+                        (ClasspathEntryProvider) info);
             }
             else {
                 ClassLoaderFactory.setupClassLoader(rootArtifact, dependencies);
             }
         }
+        return artifact;
+    }
 
+    private Throwable extractRootCause(Throwable t) {
+        if (t instanceof InvocationTargetException) {
+            return extractRootCause(((InvocationTargetException) t).getTargetException());
+        }
+        else if (t instanceof CommandException) {
+            return t;
+        }
+        else if (t instanceof RuntimeException) {
+            if (t.getCause() != null) {
+                return extractRootCause(t.getCause());
+            }
+        }
+        return t;
+    }
+
+    private List<URI> getZipDependencies(List<ArtifactDescriptor> dependencies) {
+        return dependencies.stream().map(ad -> new File(ad.uri()))
+                .filter(f -> f.getName().endsWith(".zip")).map(File::toURI)
+                .collect(Collectors.toList());
+    }
+
+    private int invokeCommand(String[] args, ArtifactDescriptor artifact,
+            List<ArtifactDescriptor> dependencies, Timing timing) {
+
+        if (timing == null) {
+            timing = new Timing();
+        }
+
+        CommandLine commandLine = null;
+        try {
+            commandLine = CommandUtils.parseCommandline(args, registry);
+            CommandInfo info = registry.findCommand(commandLine);
+
+            if (commandLine.hasOption("?") || commandLine.hasOption("h")) {
+                printCommandHelp(commandLine);
+                return 0;
+            }
+            else {
+                invokeReflectiveCommand(args, artifact, dependencies, info);
+            }
+        }
+        catch (Throwable e) {
+            // Extract root exception; cycle through nested exceptions to extract root cause
+            e = extractRootCause(e);
+
+            // Print stacktraces only if requested from the command line
+            if (commandLine != null && commandLine.hasOption('X')) {
+                log.error(e);
+            }
+            else {
+                log.error(e.getMessage());
+            }
+            return 1;
+        }
+        finally {
+            if (commandLine != null && commandLine.hasOption('t')) {
+                printTimer(timing);
+            }
+        }
+        return 0;
+    }
+
+    private void invokeReflectiveCommand(String[] args, ArtifactDescriptor artifact,
+            List<ArtifactDescriptor> dependencies, CommandInfo info) {
         try {
             // Invoke the run method on the command class
             new ReflectiveCommandRunMethodRunner().invokeCommand(artifact, info, args,
                     getZipDependencies(dependencies));
         }
         catch (NoClassDefFoundError | ClassNotFoundException e) {
-            throw new RunnerException(
-                    "Could not execute command. Likely because specified archive is missing required dependencies",
-                    e);
+            throw new RunnerException(e);
         }
         catch (RunnerException e) {
             throw e;
@@ -83,11 +201,14 @@ public class ReflectiveCommandRunner {
             throw new RunnerException(e);
         }
     }
-    
-    private List<URI> getZipDependencies(List<ArtifactDescriptor> dependencies) {
-        return dependencies.stream().map(ad -> new File(ad.uri()))
-                .filter(f -> f.getName().endsWith(".zip")).map(File::toURI)
-                .collect(Collectors.toList());
+
+    private void printCommandHelp(CommandLine commandLine) {
+        new Log(getClass()).info(
+                new CommandHelpFormatter().printCommandHelp(registry.findCommand(commandLine)));
+    }
+
+    private void printTimer(Timing timing) {
+        log.info("Command completed in " + timing.duration() + "s");
     }
 
     private List<ArtifactDescriptor> resolveDependencies(ArtifactDescriptor artifact,
@@ -106,5 +227,4 @@ public class ReflectiveCommandRunner {
         }
     }
 
-    
 }
---
+++ src/main/java/com/atomist/rug/cli/command/shell/ShellCommand.java
@@ -0,0 +1,75 @@
+package com.atomist.rug.cli.command.shell;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.atomist.rug.cli.command.AbstractAnnotationBasedCommand;
+import com.atomist.rug.cli.command.CommandEventListener;
+import com.atomist.rug.cli.command.CommandEventListenerRegistry;
+import com.atomist.rug.cli.command.annotation.Command;
+import com.atomist.rug.cli.output.ProgressReportingOperationRunner;
+import com.atomist.rug.cli.utils.ArtifactDescriptorUtils;
+import com.atomist.rug.cli.version.VersionUtils;
+import com.atomist.rug.loader.OperationsAndHandlers;
+import com.atomist.rug.metadata.MetadataWriter;
+import com.atomist.rug.resolver.ArtifactDescriptor;
+import com.atomist.source.ArtifactSource;
+import com.atomist.source.FileArtifact;
+
+public class ShellCommand extends AbstractAnnotationBasedCommand {
+
+    private static final String banner = "  ____                 ____ _     ___ \n"
+            + " |  _ \\ _   _  __ _   / ___| |   |_ _|\n"
+            + " | |_) | | | |/ _` | | |   | |    | | \n"
+            + " |  _ <| |_| | (_| | | |___| |___ | | \n"
+            + " |_| \\_\\\\__,_|\\__, |  \\____|_____|___|\n" + "              |___/ %s";
+
+    @Command
+    public void run(ArtifactSource source, ArtifactDescriptor artifact,
+            OperationsAndHandlers operations) {
+        new ProgressReportingOperationRunner<Void>(String.format("Initializing shell for %s",
+                ArtifactDescriptorUtils.coordinates(artifact))).run((reporter) -> {
+
+                    log.newline();
+                    log.info(banner, StringUtils
+                            .leftPad(VersionUtils.readVersion().orElse("0.0.0").split("-")[0], 18));
+                    log.newline();
+
+                    CommandEventListener listener = new OperationsLoadedEventListener(source,
+                            artifact);
+                    listener.operationsLoaded(operations);
+                    CommandEventListenerRegistry.register(listener);
+                    return null;
+                });
+    }
+
+    private static class OperationsLoadedEventListener implements CommandEventListener {
+
+        private ArtifactDescriptor artifact;
+        private ArtifactSource source;
+
+        public OperationsLoadedEventListener(ArtifactSource source, ArtifactDescriptor artifact) {
+            this.artifact = artifact;
+            this.source = source;
+        }
+
+        @Override
+        public void operationsLoaded(OperationsAndHandlers operations) {
+
+            FileArtifact file = MetadataWriter.create(operations, artifact, source, null);
+
+            try {
+                FileUtils.write(ShellUtils.SHELL_OPERATIONS, file.content(),
+                        StandardCharsets.ISO_8859_1);
+                FileUtils.forceDeleteOnExit(ShellUtils.SHELL_OPERATIONS);
+            }
+            catch (IOException e) {
+                // We can't write the operations out to a file, so what?
+            }
+        }
+    }
+
+}
