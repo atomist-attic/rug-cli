2727bbdcbbd6420a40041b1881ba6ad0ff9f0fdb
Christian Dupuis <cd@atomist.com>
2017-02-13 20:56:51 +0100

    Properly line break mutline printlns

--- src/main/java/com/atomist/rug/cli/output/SpinningProgressReporter.java
+++ src/main/java/com/atomist/rug/cli/output/SpinningProgressReporter.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class SpinningProgressReporter extends Thread implements ProgressReporter {
 
-    //    private static final String animNix = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏";
     private static final String animNix = org.apache.commons.lang3.StringUtils.reverse("⣾⣽⣻⢿⡿⣟⣯⣷");
     private static final String animWin32 = "|/-\\";
     private static final boolean isWindows = System.getProperty("os.name").startsWith("Windows");
@@ -43,9 +42,9 @@ public class SpinningProgressReporter extends Thread implements ProgressReporter
     @Override
     public void report(String message) {
         String[] messages = message.split("\n");
-        for (String message1 : messages) {
-            if (message1 != "") {
-                additionalMessages.offer(message1);
+        for (String msg : messages) {
+            if (msg != "") {
+                additionalMessages.offer(msg);
             }
         }
     }
