8ad863b25d7644f63ccab6c5ad6a48d084ad94da
Christian Dupuis <cd@atomist.com>
2017-02-14 23:11:30 +0100

    Move path expression evaluation into reporter

--- src/main/java/com/atomist/rug/cli/command/tree/TreeCommand.java
+++ src/main/java/com/atomist/rug/cli/command/tree/TreeCommand.java
@@ -10,6 +10,7 @@ import com.atomist.rug.cli.command.annotation.Argument;
 import com.atomist.rug.cli.command.annotation.Command;
 import com.atomist.rug.cli.command.annotation.Option;
 import com.atomist.rug.cli.command.utils.ArtifactSourceUtils;
+import com.atomist.rug.cli.output.ProgressReportingOperationRunner;
 import com.atomist.rug.cli.output.Style;
 import com.atomist.rug.cli.tree.LogVisitor;
 import com.atomist.rug.cli.tree.TreeNodeTreeCreator;
@@ -35,20 +36,25 @@ public class TreeCommand extends AbstractAnnotationBasedCommand {
     @Command
     public void run(@Argument(index = 1, defaultValue = "") String expression,
             @Option("change-dir") String rootName, @Option("values") boolean values) {
+        
+        Collection<TreeNode> treeNodes = new ProgressReportingOperationRunner<Collection<TreeNode>>(
+                "Evaluating path expression against project").run((indicator) -> {
 
-        PathExpression pathExpression = PathExpressionParser$.MODULE$.parseString(expression);
+                    PathExpression pathExpression = PathExpressionParser$.MODULE$
+                            .parseString(expression);
 
-        File root = FileUtils.createProjectRoot(rootName);
-        ArtifactSource source = ArtifactSourceUtils.createArtifactSource(root);
+                    File root = FileUtils.createProjectRoot(rootName);
+                    ArtifactSource source = ArtifactSourceUtils.createArtifactSource(root);
 
-        ExpressionEngine pxe = new PathExpressionEngine();
-        TreeNode pmv = new ProjectMutableView(new EmptyArtifactSource(""), source);
+                    ExpressionEngine pxe = new PathExpressionEngine();
+                    TreeNode pmv = new ProjectMutableView(new EmptyArtifactSource(""), source);
 
-        Either<String, List<TreeNode>> result = pxe.evaluate(pmv, pathExpression,
-                DefaultTypeRegistry$.MODULE$, Option$.MODULE$.apply(null));
+                    Either<String, List<TreeNode>> result = pxe.evaluate(pmv, pathExpression,
+                            DefaultTypeRegistry$.MODULE$, Option$.MODULE$.apply(null));
 
-        Collection<TreeNode> treeNodes = JavaConverters
-                .asJavaCollectionConverter(result.right().get()).asJavaCollection();
+                    return JavaConverters.asJavaCollectionConverter(result.right().get())
+                            .asJavaCollection();
+                });
 
         log.newline();
         log.info(Style.cyan(Constants.DIVIDER) + " " + Style.bold("Path Expression"));
