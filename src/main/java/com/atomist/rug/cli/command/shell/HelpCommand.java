c5fccc889aa09f713ecd5510336b967e39468874
Christian Dupuis <cd@atomist.com>
2017-02-15 15:53:06 +0100

    Fix rug command invocation
    fixes #109

--- src/main/java/com/atomist/rug/cli/command/shell/HelpCommand.java
+++ src/main/java/com/atomist/rug/cli/command/shell/HelpCommand.java
@@ -1,15 +1,22 @@
 package com.atomist.rug.cli.command.shell;
 
-import com.atomist.rug.cli.command.AbstractAnnotationBasedCommand;
+import java.net.URI;
+
+import org.apache.commons.cli.CommandLine;
+
+import com.atomist.rug.cli.Log;
+import com.atomist.rug.cli.command.AbstractCommand;
 import com.atomist.rug.cli.command.CommandUtils;
 import com.atomist.rug.cli.command.ServiceLoadingCommandInfoRegistry;
-import com.atomist.rug.cli.command.annotation.Command;
 import com.atomist.rug.cli.command.utils.CommandHelpFormatter;
+import com.atomist.rug.resolver.ArtifactDescriptor;
 
-public class HelpCommand extends AbstractAnnotationBasedCommand {
+public class HelpCommand extends AbstractCommand {
 
-    @Command
-    public void run() {
+    private static final Log log = new Log(HelpCommand.class);
+    
+    @Override
+    protected void run(URI[] uri, ArtifactDescriptor artifact, CommandLine commandLine) {
         log.info(new CommandHelpFormatter().printHelp(new ServiceLoadingCommandInfoRegistry(),
                 CommandUtils.options()));
     }
