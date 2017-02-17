cf49db36392f228f3977e3371c0e7c40c6c2bb5a
Christian Dupuis <cd@atomist.com>
2017-02-17 15:38:15 +0100

    Provide tab completion for archive names

---
+++ src/main/java/com/atomist/rug/cli/command/shell/ArchiveNameCompleter.java
@@ -0,0 +1,60 @@
+package com.atomist.rug.cli.command.shell;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.IOUtils;
+import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.ParsedLine;
+
+public class ArchiveNameCompleter implements Completer {
+
+    private static final List<String> COMMANDS = Arrays.asList("load", "shell", "repl", "sh");
+
+    @Override
+    public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
+        if (line.words().size() == 2) {
+            String cmd = line.words().get(0);
+            String word = line.word();
+            if (COMMANDS.contains(cmd)) {
+                if (word.contains(":")) {
+                    String group = word.split(":")[0] + ":";
+                    archivesFromCache().stream().filter(a -> a.startsWith(group))
+                            .collect(Collectors.toSet()).forEach(a -> candidates
+                                    .add(new Candidate(a, a, "Archives", null, null, null, true)));
+                }
+                else {
+                    archivesFromCache().stream().map(a -> a.split(":")[0])
+                            .collect(Collectors.toSet()).forEach(a -> candidates
+                                    .add(new Candidate(a + ":", a, "Archives", null, null, null, false)));
+                }
+            }
+        }
+    }
+
+    private List<String> archivesFromCache() {
+        List<String> archives = new ArrayList<>();
+        if (ShellUtils.SHELL_ARCHIVES.exists()) {
+            try {
+                IOUtils.lineIterator(new FileInputStream(ShellUtils.SHELL_ARCHIVES),
+                        StandardCharsets.UTF_8).forEachRemaining((l) -> archives.add(l));
+            }
+            catch (IOException e) {
+            }
+        }
+        return archives;
+    }
+
+    // private List<String> archivesFromRepository() {
+    // List<String> archives = new ArrayList<>();
+    //
+    // }
+
+}
