fefd29e3bc8bf5b59b5289b8d912b0d2c1c7e350
Christian Dupuis <cd@atomist.com>
2017-02-20 14:15:56 +0100

    Filter out pref store log messages

--- src/main/java/com/atomist/rug/cli/output/FilteringPrintStream.java
+++ src/main/java/com/atomist/rug/cli/output/FilteringPrintStream.java
@@ -3,14 +3,14 @@ package com.atomist.rug.cli.output;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
-public class Slf4jFilteringPrintStream extends PrintStream {
+public class FilteringPrintStream extends PrintStream {
 
-    public Slf4jFilteringPrintStream(OutputStream out) {
+    public FilteringPrintStream(OutputStream out) {
         super(out);
     }
 
     public void println(String l) {
-        if (!l.startsWith("SLF4J")) {
+        if (!l.startsWith("SLF4J") && !l.startsWith("INFO:")) {
             super.println(l);
         }
     }
