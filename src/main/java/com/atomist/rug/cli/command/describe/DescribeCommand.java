7ae0c04a7c6c3fcc0fe5322a190ecf109c140adb
Christian Dupuis <cd@atomist.com>
2017-03-04 14:33:27 -0800

    Capitalize description in describe

--- src/main/java/com/atomist/rug/cli/command/describe/DescribeCommand.java
+++ src/main/java/com/atomist/rug/cli/command/describe/DescribeCommand.java
@@ -347,7 +347,9 @@ public class DescribeCommand extends AbstractAnnotationBasedCommand {
             if (!ops.isEmpty()) {
                 ops.forEach(e -> log.info("  "
                         + Style.yellow(StringUtils.stripName(e.name(), artifact)) + "\n    "
-                        + WordUtils.wrap(e.description(), Constants.WRAP_LENGTH, "\n    ", false)));
+                        + WordUtils.wrap(
+                                org.apache.commons.lang3.StringUtils.capitalize(e.description()),
+                                Constants.WRAP_LENGTH, "\n    ", false)));
             }
             else {
                 log.info(Style.yellow("  No %s found", labels.label.toLowerCase()));
@@ -441,8 +443,7 @@ public class DescribeCommand extends AbstractAnnotationBasedCommand {
     private void describeSecrets(SecretAwareRug rug) {
         if (!rug.secrets().isEmpty()) {
             log.info(Style.cyan(Constants.DIVIDER) + " " + Style.bold("Secrets"));
-            asJavaCollection(rug.secrets()).forEach(
-                    t -> log.info("  " + Style.yellow(t.name())));
+            asJavaCollection(rug.secrets()).forEach(t -> log.info("  " + Style.yellow(t.name())));
         }
     }
 
@@ -453,7 +454,6 @@ public class DescribeCommand extends AbstractAnnotationBasedCommand {
                     t -> log.info("  " + Style.yellow(t.localKey()) + " => " + t.foreignKey()));
         }
     }
-
 
     private void describeTags(Rug info) {
         if (!info.tags().isEmpty()) {
