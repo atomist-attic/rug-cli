95837bb1ef4c72c35a66009ad3616a5705bc37c7
David Dooling <dooling@gmail.com>
2017-03-09 10:27:49 -0600

    Generate Markdown CLI reference documentation (#154)
    Add MkDocs class and tests to generate Markdown reference
    documentation for the CLI.  Documentation is in the README.
    
    Closes #151

--- src/main/java/com/atomist/rug/cli/command/CommandUtils.java
+++ src/main/java/com/atomist/rug/cli/command/CommandUtils.java
@@ -5,19 +5,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.*;
 
 import com.atomist.rug.cli.Constants;
 import com.atomist.rug.cli.command.utils.ParseExceptionProcessor;
