0cd386be8aefb44f0ae4b60ce744f2d39da5133c
Christian Dupuis <cd@atomist.com>
2017-03-10 16:21:47 +0100

    Update supported version range to 0.13.0

--- src/main/java/com/atomist/rug/cli/Constants.java
+++ src/main/java/com/atomist/rug/cli/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static final String ARTIFACT = "rug-cli";
 
     public static final String RUG_ARTIFACT = "rug";
-    public static final String RUG_VERSION_RANGE = "[0.12.0,1.0.0)";
+    public static final String RUG_VERSION_RANGE = "[0.13.0,1.0.0)";
 
     public static final String ATOMIST_ROOT = ".atomist";
     public static final String CLI_CONFIG_NAME = "cli.yml";
