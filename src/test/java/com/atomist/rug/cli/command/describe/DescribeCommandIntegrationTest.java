ee3988afb07cc06dddc441fb9e3622e74d3349c8
Christian Dupuis <cd@atomist.com>
2017-04-14 00:53:38 +0200

    Fix test

--- src/test/java/com/atomist/rug/cli/command/describe/DescribeCommandIntegrationTest.java
+++ src/test/java/com/atomist/rug/cli/command/describe/DescribeCommandIntegrationTest.java
@@ -127,7 +127,7 @@ public class DescribeCommandIntegrationTest extends AbstractCommandTest {
     @Test
     public void testUnSuccessfulDescribe() throws Exception {
         assertFailure(
-                "com.atomist.rug.cli.command.CommandException: Invalid TYPE provided. Please tell me what you would like to describe: archive, editor, generator, reviewer, command-handler, event-handler, or response-handler \n"
+                "com.atomist.rug.cli.command.CommandException: Invalid TYPE provided. Please tell me what you would like to describe: archive, editor, generator, reviewer, command-handler, event-handler, response-handler or function \n"
                         + "\n" + "Run the following command for usage help:\n"
                         + "  rug describe --help",
                 "describe", "-l");
