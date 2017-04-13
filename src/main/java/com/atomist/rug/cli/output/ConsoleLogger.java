7c8302d19805340d6d9e4fcba8d3a9ece6c75de0
Christian Dupuis <cd@atomist.com>
2017-04-13 09:13:09 +0200

    Fix ConsoleLogger indentation issue

--- src/main/java/com/atomist/rug/cli/output/ConsoleLogger.java
+++ src/main/java/com/atomist/rug/cli/output/ConsoleLogger.java
@@ -4,6 +4,7 @@ import javax.script.Bindings;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 
+import com.atomist.rug.cli.Constants;
 import com.atomist.rug.cli.Log;
 
 import scala.runtime.AbstractFunction1;
@@ -11,30 +12,40 @@ import scala.runtime.BoxedUnit;
 
 public class ConsoleLogger {
     
-    private Log log = new Log(ConsoleLogger.class);
+    private static Log log = new Log(ConsoleLogger.class);
+
+    private String indent = Constants.LEFT_PADDING;
+    
+    private ConsoleLogger(String indent) {
+        this.indent = indent;
+    }
     
     public void log(String msg) {
-        log.info(msg);
+        log.info(indent + msg);
     }
     
     public void warn(String msg) {
-        log.info(msg);
+        log.info(indent + msg);
     }
     
     public void error(String msg) {
-        log.error(msg);
+        log.error(indent + msg);
     }
     
-    public static AbstractFunction1<ScriptEngine, BoxedUnit> consoleLogger() {
+    public static AbstractFunction1<ScriptEngine, BoxedUnit> consoleLogger(String indent) {
         return new AbstractFunction1<ScriptEngine, BoxedUnit>() {
 
             @Override
             public BoxedUnit apply(ScriptEngine engine) {
                 Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
-                bindings.put("console", new ConsoleLogger());
+                bindings.put("console", new ConsoleLogger(indent));
                 engine.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
                 return null;
             }
         };
+    }
+    
+    public static AbstractFunction1<ScriptEngine, BoxedUnit> consoleLogger() {
+        return consoleLogger(Constants.LEFT_PADDING);
     }
  }
