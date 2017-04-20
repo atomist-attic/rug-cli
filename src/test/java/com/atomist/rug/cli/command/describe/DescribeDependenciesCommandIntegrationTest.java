cbf5a3684ce700df1865fda11182a11deb741415
Christian Dupuis <cd@atomist.com>
2017-04-20 17:41:12 +0200

    Fix remaining describe dependencies test

--- src/test/java/com/atomist/rug/cli/command/describe/DescribeDependenciesCommandIntegrationTest.java
+++ src/test/java/com/atomist/rug/cli/command/describe/DescribeDependenciesCommandIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.atomist.rug.cli.command.dependencies;
+package com.atomist.rug.cli.command.describe;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -6,7 +6,7 @@ import org.junit.Test;
 import com.atomist.rug.cli.AbstractCommandTest;
 import com.atomist.rug.cli.Constants;
 
-public class DependenciesCommandIntegrationTest extends AbstractCommandTest {
+public class DescribeDependenciesCommandIntegrationTest extends AbstractCommandTest {
     
     @Before
     public void before() throws Exception {
@@ -15,19 +15,19 @@ public class DependenciesCommandIntegrationTest extends AbstractCommandTest {
 
     @Test
     public void testHelp() throws Exception {
-        assertSuccess("Usage: rug dependencies [OPTION]... ARTIFACT", "dependencies", "-h");
+        assertSuccess("Usage: rug describe [OPTION]... TYPE ARTIFACT", "describe", "dependencies", "-h");
     }
 
     @Test
     public void testSuccessfulGeneratorDescribeWithVersion() throws Exception {
         assertSuccess("atomist-rugs:spring-boot-rest-service (0.10.0" + Constants.DOT + "zip)",
-                "deps",
+                "describe", "dependencies",
                 "atomist-rugs:spring-boot-rest-service:NewSpringBootRestService", "-a", "0.10.0");
     }
 
     @Test
     public void testSuccessfulDependencies() throws Exception {
-        assertSuccess("rug-cli-tests:handlers (0.12.0)", "deps",
+        assertSuccess("rug-cli-tests:handlers (0.12.0)", "describe", "dependencies",
                 "-l", "--operations");
     }
 
