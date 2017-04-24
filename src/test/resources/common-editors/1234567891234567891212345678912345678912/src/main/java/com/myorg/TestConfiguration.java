2e95d8613c6ff1532fa2fa6609d2ea0abb60ac97
Christian Dupuis <cd@atomist.com>
2017-04-24 17:23:39 +0200

    Delete test data

--- src/test/resources/common-editors/1234567891234567891212345678912345678912/src/main/java/com/myorg/TestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.myorg;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TestConfiguration {
-    // Your beans go here ...
-    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
-}
